9738aed603efcf0be6ec098350d0d12f74618624
Albertooo18 <117312330+Albertooo18@users.noreply.github.com>
2024-07-02 17:44:04 -0500

    Delete model directory

--- model/converter.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-import tensorflowjs as tfjs
-
-# Load the model from the .h5 file
-model = tf.keras.models.load_model(r'C:\Users\xcrss\OneDrive - Universidad Tecnológica de Panamá\C 2\C\Code\Samsung\Proyecto_Tokyo\Tokyo_repo\model\modelo.h5')
-
-# Convert the model to TensorFlow.js format and save it in the specified folder
-tfjs.converters.save_keras_model(model, 'output')
